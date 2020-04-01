c8f41363f96ffa87cee763b048c3e514b47a43ea
wahyuekamulatsari <wahyuekamalik@gmail.com>
2020-04-01 19:19:16 +0700

    membuat routes

--- controller.js
+++ controller.js
@@ -4,5 +4,5 @@ var response = require('./res');
 var connection = require('./koneksi');
 
 exports.index= function(req, res) {
-    response.ok("Aplikasi REST API ku berjalan!")
+    response.ok("Aplikasi REST API ku berjalan!", res)
 };
