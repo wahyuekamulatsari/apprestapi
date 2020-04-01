a1ca94f6f6115ded1e2105c85d837daad5c76100
wahyuekamulatsari <wahyuekamalik@gmail.com>
2020-04-01 19:08:34 +0700

    setting routes di server js

--- server.js
+++ server.js
@@ -2,6 +2,15 @@ const express = require ('express');
 const bodyParser = require ('body-parser');
 const app = express();
 
+
+//parse app
+app.use(bodyParser.urlencoded( {extended}));
+app.use(bodyParser.json());
+
+//panggil routes
+var routes = require('./routes');
+routes(app);
+
 //parser application/json
 app.use(bodyParser.urlencoded({extended}));
 app.use(bodyParser.json());
