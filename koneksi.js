e2804aadad8fb03f8d2d69921a99ab969be8d522
wahyuekamulatsari <wahyuekamalik@gmail.com>
2020-04-01 18:39:36 +0700

    menambahkan file koneksi

---
+++ koneksi.js
@@ -0,0 +1,16 @@
+var mysql = require ('mysql');
+
+const conn = mysql.createConnection( {
+    host: 'localhost',
+    user:'root',
+    password: '',
+    database:'dbrestapi'
+});
+
+conn.connect((err)=> {
+    if(err) throw err;
+    console.log('Mysql terkoneksi');
+
+});
+
+module.exports = conn;
